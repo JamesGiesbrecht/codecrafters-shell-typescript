90650eaf73921de37aeafd6fe92fcc7884524480
James Giesbrecht <giesbrechtjames@gmail.com>
2025-11-09 11:55:10 -0600

    Inital repl loop

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,27 @@
 import { createInterface } from "readline";
 
+// Create interface
 const rl = createInterface({
   input: process.stdin,
   output: process.stdout,
+  prompt: "$ ",
 });
 
-rl.question("$ ", (command) => {
+const executeCommand = (command: string) => {
   console.log(`${command}: command not found`);
-  rl.close();
-});
+};
+
+const main = () => {
+  // Start prompt
+  rl.prompt();
+
+  // Handle input lines
+  rl.on("line", (command) => {
+    executeCommand(command);
+    rl.prompt();
+  }).on("close", () => {
+    process.exit(0);
+  });
+};
+
+main();
