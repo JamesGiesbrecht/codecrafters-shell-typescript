dac0f6493fc9974c4b77167e63be6dc07771ed47
James Giesbrecht <giesbrechtjames@gmail.com>
2025-11-09 16:36:49 -0600

    type builtin

--- app/builtins.ts
+++ app/builtins.ts
@@ -8,6 +8,14 @@ const builtins: Record<string, BuiltinCommand> = {
   echo: (args: string[]) => {
     console.log(args.join(" "));
   },
+  type: (args: string[]) => {
+    const command = args[0];
+    if (builtins[command]) {
+      console.log(`${command} is a shell builtin`);
+    } else {
+      console.log(`${command}: not found`);
+    }
+  },
 };
 
 export default builtins;
