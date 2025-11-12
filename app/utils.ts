990a5bced83fbafb92c080545266f6e43ace1018
James Giesbrecht <giesbrechtjames@gmail.com>
2025-11-12 15:02:36 -0600

    Preserving quoted segments in commands

--- app/utils.ts
+++ app/utils.ts
@@ -1,11 +1,19 @@
 import fs from "fs";
 import path from "path";
 
+const REGEX = {
+  spaces: /\s+/,
+  singleQuotes: /\s+(?=(?:[^']*'[^']*')*[^']*$)/, // Matches spaces not contained in single quotes
+};
+
 const parseInput = function (input: string): {
   command: string;
   args: string[];
 } {
-  const tokens = input.trim().split(/\s+/);
+  const tokens = input
+    .trim()
+    .split(REGEX.singleQuotes)
+    .map((token) => token.replaceAll("'", "")); // Remove quotes '
   const command = tokens[0];
   const args = tokens.slice(1);
   return { command, args };
