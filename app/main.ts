e1b44b6668f748cceb626f6619b2e0b0bf323774
James Giesbrecht <giesbrechtjames@gmail.com>
2025-11-18 13:49:20 -0600

    Multi result tab completion

--- app/main.ts
+++ app/main.ts
@@ -4,28 +4,52 @@ import ReplHandler from "./handlers/ReplHandler";
 import FileHelper from "./helpers/FileHelper";
 import builtins from "./util/builtins";
 
-const completer = (line: string) => {
-  // Get command names add spaces to the end of the command
-  const completions = Object.keys(builtins)
-    .concat(FileHelper.getExecutablesInPath())
-    .map((c) => `${c.toLowerCase()} `);
-  // Find partial matches
-  const hits = completions.filter((c) => c.startsWith(line.toLowerCase()));
-  if (hits.length) {
-    return [hits, line];
-  }
-  process.stdout.write("\x07");
-  return [completions, line];
+const beepSignal = () => {
+  process.stdout.write("\u0007");
 };
 
-const rl = createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  prompt: CONSTANTS.PROMPT_PREFIX,
-  completer,
-});
-
 const repl = function () {
+  let tabCounter = 0;
+
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: CONSTANTS.PROMPT_PREFIX,
+    completer: (line: string) => {
+      // Get command names add spaces to the end of the command
+      const completions = Object.keys(builtins)
+        .concat(FileHelper.getExecutablesInPath())
+        .map((c) => `${c.toLowerCase()} `)
+        .sort();
+
+      // Find partial matches
+      const hits = Array.from(
+        new Set(completions.filter((c) => c.startsWith(line.toLowerCase())))
+      );
+      if (hits.length === 0) {
+        beepSignal();
+        return [completions, line];
+      }
+      // Return the first hit
+      if (hits.length === 1) {
+        return [hits, line];
+      }
+      // If there are multiple matches, print them out and write the original line to the screen
+      if (hits.length > 1) {
+        if (tabCounter === 0) {
+          tabCounter += 1;
+          beepSignal();
+        } else {
+          process.stdout.write(
+            `\n${hits.join(" ")}\n${CONSTANTS.PROMPT_PREFIX}${line}`
+          );
+          tabCounter = 0;
+        }
+        return [[], line];
+      }
+      return [hits, line];
+    },
+  });
   rl.prompt();
 
   rl.on("line", (fullCommand) => {
